e75fef77d14b9a9226b582cfdb02dcc03462ec4f
Wes <wmont012@ucr.edu>
2024-05-05 18:02:45 -0700

    Added Hero section

---
+++ site/src/components/Hero.js
@@ -0,0 +1,13 @@
+import './Hero.css';
+
+export default function Hero(){
+    return (
+        <div id = "hero">
+            <img id = "my-pic" src = "https://media.licdn.com/dms/image/D5603AQHgfF9t-5pvCg/profile-displayphoto-shrink_800_800/0/1686001697096?e=2147483647&v=beta&t=osq-DzTH5wDhOb6QFXcMjG2Pbs6kHXmKCqQqmpCwHu0" alt = "me"/>
+            <div id = "hero-text">
+                <h1 class = "name">Westin Montano</h1>
+                <h2 class = "title">Game Developer and Lecturer at UCR</h2>
+            </div>
+        </div>
+    );
+}
